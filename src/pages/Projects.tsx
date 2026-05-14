467a0f8eb6c104987dfd8e379887b8b018557ecb
Tulio <tuliorangeldesigner@gmail.com>
2026-05-14 11:03:00 -0300

    Use two by two thumbnail preview grid

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -472,7 +472,7 @@ const Projects = () => {
               </div>
 
               <div className="lg:col-span-7 p-4 md:p-6 bg-foreground/[0.03]">
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-4">
+                <div className="grid grid-cols-2 gap-3 md:gap-4">
                   {thumbnails.slice(0, 4).map((thumb, index) => (
                     <div
                       key={thumb.id}
